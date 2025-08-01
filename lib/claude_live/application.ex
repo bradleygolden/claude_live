c913bbadd59665a6e6b670a8b6deb1bed2ab7587
Bradley Golden <golden.bradley@gmail.com>
2025-08-01 13:52:50 -0500

    Remove non-critical comments from codebase
    - Removed explanatory comments from configuration files
    - Cleaned up router comments
    - Removed boilerplate comments from application and mix files
    - Kept only essential documentation like @moduledoc

--- lib/claude_live/application.ex
+++ lib/claude_live/application.ex
@@ -1,6 +1,4 @@
 defmodule ClaudeLive.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -17,23 +15,15 @@ defmodule ClaudeLive.Application do
          Application.fetch_env!(:claude_live, Oban)
        )},
       {Phoenix.PubSub, name: ClaudeLive.PubSub},
-      # Terminal infrastructure
       {Registry, keys: :unique, name: ClaudeLive.Terminal.Registry},
       ClaudeLive.Terminal.Supervisor,
-      # Start a worker by calling: ClaudeLive.Worker.start_link(arg)
-      # {ClaudeLive.Worker, arg},
-      # Start to serve requests, typically the last entry
       ClaudeLiveWeb.Endpoint
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ClaudeLive.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     ClaudeLiveWeb.Endpoint.config_change(changed, removed)
