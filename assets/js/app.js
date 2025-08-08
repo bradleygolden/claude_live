962fd49eb3bd5219c1f98a3f479f2be1d3711a8a
Bradley Golden <golden.bradley@gmail.com>
2025-08-08 15:56:18 -0500

    Revert "Fix terminal auto-focus when navigating from dashboard"
    This reverts commit fc015b2f6e349856ac9cbd2bef12b2551e4abf0f.

--- assets/js/app.js
+++ assets/js/app.js
@@ -75,7 +75,6 @@ const SingleTerminalHook = {
     }
     
     this.terminalId = terminalId
-    this.hasInitialized = false
     
     this.handleEvent("terminal_output", ({ data }) => {
       this.terminalManager.writeToTerminal(this.terminalId, data)
@@ -89,30 +88,8 @@ const SingleTerminalHook = {
       this.terminalManager.handleTerminalClosed(this.terminalId)
     })
     
-    this.handleEvent("auto-focus-terminal", ({ terminal_id }) => {
-      console.log('Auto-focus event received for terminal:', terminal_id)
-      setTimeout(() => {
-        if (this.terminalManager.terminals[terminal_id]) {
-          const terminal = this.terminalManager.terminals[terminal_id]
-          console.log('Focusing terminal from auto-focus event:', terminal_id)
-          terminal.focus()
-        } else {
-          console.log('Terminal not found for auto-focus, initializing:', terminal_id)
-          this.terminalManager.initTerminal(terminal_id)
-          setTimeout(() => {
-            if (this.terminalManager.terminals[terminal_id]) {
-              this.terminalManager.terminals[terminal_id].focus()
-            }
-          }, 200)
-        }
-      }, 100)
-    })
-    
-    // Auto-initialize terminal on mount
     setTimeout(() => {
-      if (this.terminalId && !this.hasInitialized) {
-        this.hasInitialized = true
-        console.log('Auto-initializing terminal:', this.terminalId)
+      if (this.terminalId) {
         this.terminalManager.initTerminal(this.terminalId)
       }
     }, 100)
